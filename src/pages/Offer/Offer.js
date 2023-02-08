ae015610bbebb0862aeef6d0a592fee59e4e853c
loloexotic <loloexotic@airdeloloexotic.lan>
2023-02-08 15:15:37 +0100

    link

---
+++ src/pages/Offer/Offer.js
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+const Offer = () => {
+  return (
+    <div>
+      <h1>yes Offer</h1>
+      <Link to="/">Home</Link>
+      <Link to="/home">Aller vers la page Home</Link>
+    </div>
+  );
+};
