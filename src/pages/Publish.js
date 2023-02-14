570886d8e96d6b949284e47182d1e7ad3019c710
loloexotic <loloexotic@MacBook-Air-de-loloexotic.local>
2023-02-14 21:00:07 +0100

    update

--- src/pages/Publish.js
+++ src/pages/Publish.js
@@ -1,68 +1,135 @@
 import axios from "axios";
 import { useState } from "react";
-import { Link, Navigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
 const Publish = () => {
-  const [file, setFile] = useState({});
   const [picture, setPicture] = useState();
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [brand, setBrand] = useState("");
+  const [size, setSize] = useState("");
+  const [color, setColor] = useState("");
+  const [condition, setCondition] = useState("");
+  const [place, setPlace] = useState("");
+  const [price, setPrice] = useState("");
 
-  const Token = async () => {
-    const response = await axios.post(
-      "https://lereacteur-vinted-api.herokuapp.com/offer/publish",
-      params,
-      {
-        headers: {
-          authorization: `Bearer ${token}`,
-        },
-      }
-    );
+  const handleToken = async (event) => {
+    event.preventDefault();
+    try {
+      const formData = new FormData();
+      formData.append("picture", picture);
+      formData.append("title", title);
+      formData.append("description", description);
+      formData.append("brand", brand);
+      formData.append("size", size);
+      formData.append("color", color);
+      formData.append("condition", condition);
+      formData.append("city", place);
+      formData.append("price", price);
+
+      const response = await axios.post(
+        "https://lereacteur-vinted-api.herokuapp.com/offer/publish",
+        formData,
+        {
+          headers: {
+            authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      console.log(response.data);
+    } catch (error) {
+      console.log(error.response.data);
+    }
   };
 
-  return Token ? (
+  return token ? (
     <div>
       <form>
         <input
           type="file"
-          onChange={(event) => {
-            setPicture(event.target.files[0]);
+          onChange={(e) => {
+            setPicture(e.target.files[0]);
           }}
         />
         <section>
           <label htmlFor="Titre">
             Titre
-            <input type="text" />
+            <input
+              type="text"
+              onChange={(e) => {
+                setTitle(e.target.value);
+              }}
+            />
           </label>
           <label htmlFor="message">
             Décris ton article
-            <textarea name="" id="" cols="30" rows="10"></textarea>
+            <textarea
+              name=""
+              id=""
+              cols="30"
+              rows="10"
+              onChange={(e) => {
+                setDescription(e.target.value);
+              }}
+            ></textarea>
           </label>
         </section>
         <section>
           <label htmlFor="">
             Marque
-            <input type="text" />
+            <input
+              type="text"
+              onChange={(e) => {
+                setBrand(e.target.value);
+              }}
+            />
           </label>
           <label htmlFor="">
             Taille
-            <input type="text" />
+            <input
+              type="text"
+              onChange={(e) => {
+                setSize(e.target.value);
+              }}
+            />
           </label>
           <label htmlFor="">
             Couleur
-            <input type="text" />
+            <input
+              type="text"
+              onChange={(e) => {
+                setColor(e.target.value);
+              }}
+            />
           </label>
           <label htmlFor="">
             Etat
-            <input type="text" />
+            <input
+              type="text"
+              onChange={(e) => {
+                setCondition(e.target.value);
+              }}
+            />
           </label>
           <label htmlFor="">
             Lieu
-            <input type="text" />
+            <input
+              type="text"
+              onChange={(e) => {
+                setPlace(e.target.value);
+              }}
+            />
           </label>
         </section>
         <div>
           <label htmlFor="">
             Prix
-            <input type="text" />
+            <input
+              type="text"
+              onChange={(e) => {
+                setPrice(e.target.value);
+              }}
+            />
           </label>
           <input type="checkbox" />
           <span>Je suis interéssé(e) par les échanges</span>
